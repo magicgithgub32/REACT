7f679276ec32600cad97ed4f8f32cadbe4b078b5
Rubcs <rubcastellon@gmail.com>
2023-01-03 13:06:02 +0100

    add react-router to REACT

---
+++ react-router/src/App.jsx
@@ -0,0 +1,28 @@
+import { NavLink, Outlet } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <header>
+        <h1>React-Router v6</h1>
+      </header>
+      <div>
+      
+      <nav>
+        <NavLink to="">Home 🏚</NavLink>
+        <NavLink to="heroes">Heroes 🦹🏻‍♂️ </NavLink>
+        <NavLink to="about">About 🙋‍♀️</NavLink>
+       </nav>
+       </div>
+        <main>
+          <Outlet />
+        </main>
+        <footer>Created by RockTheCode with 💜</footer>
+       </div>
+       
+  )
+}
+
+export default App
